557c8651ab42112c8718c02328bd6552c0ee727d
Ekansh Saxena <ekansh005@gmail.com>
2018-09-04 23:08:51 +0200

    spelling mistake in auth token generation

--- server/server.js
+++ server/server.js
@@ -77,7 +77,7 @@ app.post('/users', (req, res) => {
   let user = new User(body);
 
   user.save().then(() => {
-    return user.generateAuthToken1();
+    return user.generateAuthToken();
   }).then((token) => {
     res.header('x-auth', token).send(user.toJSON());
   }).catch((e) => res.status(400).send(e));
